9362578e571d3a51e8181c715ac9d7db2eacb6d3
Yanick Witschi <yanick.witschi@terminal42.ch>
2025-12-19 16:57:46 +0100

    Disable ps provider on windows (#12)

--- tests/Windows/WindowsTest.php
+++ tests/Windows/WindowsTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Toflar\CronjobSupervisor\Test\Windows;
 
-use Toflar\CronjobSupervisor\Provider\PsProvider;
 use Toflar\CronjobSupervisor\Provider\WindowsTaskListProvider;
 use Toflar\CronjobSupervisor\Test\AbstractProviderTestCase;
 
